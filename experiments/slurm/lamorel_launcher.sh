4af5b7b3f3223dbdec4bc5daf944e699a2e6cdb8
Mahdi Ranjbar <mhdi.rnjbr@yahoo.com>
2025-07-09 17:32:53 +0200

    update lamorel launcher path

--- experiments/slurm/lamorel_launcher.sh
+++ experiments/slurm/lamorel_launcher.sh
@@ -3,4 +3,5 @@
 MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | sed -n "1p")
 echo "running process ${SLURM_PROCID} on node $(hostname) with master ${MASTER_ADDR}"
 export "DLP_STORAGE"='storage'
-python -m lamorel_launcher.launch lamorel_args.accelerate_args.machine_rank=$SLURM_PROCID lamorel_args.accelerate_args.main_process_ip=$MASTER_ADDR $*
+export PYTHONPATH=$PYTHONPATH:/home/mranjbar/workdir/Grounding_LLMs_with_online_RL/lamorel/lamorel/src
+/home/mranjbar/miniconda3/envs/dlp/bin/python -m lamorel_launcher.launch lamorel_args.accelerate_args.machine_rank=$SLURM_PROCID lamorel_args.accelerate_args.main_process_ip=$MASTER_ADDR $*
